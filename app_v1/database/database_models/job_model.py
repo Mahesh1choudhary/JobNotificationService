5af65a21fe8eab0a60a3a267d926dad76d279d55
aryansinha <sinhaaryan88@gmail.com>
2026-04-03 22:38:58 +0530

    refactor

--- app_v1/database/database_models/job_model.py
+++ app_v1/database/database_models/job_model.py
@@ -1,35 +1,35 @@
-
-from sqlalchemy import Column, Integer, String, DateTime, UniqueConstraint, event
-from sqlalchemy.ext.declarative import declarative_base
+#
+# from sqlalchemy import Column, Integer, String, DateTime, UniqueConstraint, event
+# from sqlalchemy.ext.declarative import declarative_base
 import hashlib
-
-Base = declarative_base()
-
+#
+# Base = declarative_base()
+#
 def compute_hash(description: str) -> str:
     return hashlib.sha256(description.encode("utf-8")).hexdigest()
-
-class JobModel(Base):
-    __tablename__ = "jobs"
-
-    id = Column(Integer, primary_key=True)
-
-    company = Column(String, nullable=False)
-    job_link = Column(String, nullable=False)
-
-    job_id = Column(String)
-    job_description = Column(String)
-
-    description_hash = Column(String)
-
-    created_at = Column(DateTime)
-
-    status = Column[str](String, nullable=False, default="pending")
-
-__table_args__ = (
-        UniqueConstraint('company', 'job_link', 'description_hash', name='uq_job_unique'),
-    )
-
-
-@event.listens_for(JobModel, "before_insert")
-def set_hash_before_insert(mapper, connection, target):
-    target.description_hash = compute_hash(target.job_description or "")
+#
+# class JobModel(Base):
+#     __tablename__ = "jobs"
+#
+#     id = Column(Integer, primary_key=True)
+#
+#     company = Column(String, nullable=False)
+#     job_link = Column(String, nullable=False)
+#
+#     job_id = Column(String)
+#     job_description = Column(String)
+#
+#     description_hash = Column(String)
+#
+#     created_at = Column(DateTime)
+#
+#     status = Column[str](String, nullable=False, default="pending")
+#
+# __table_args__ = (
+#         UniqueConstraint('company', 'job_link', 'description_hash', name='uq_job_unique'),
+#     )
+#
+#
+# @event.listens_for(JobModel, "before_insert")
+# def set_hash_before_insert(mapper, connection, target):
+#     target.description_hash = compute_hash(target.job_description or "")
