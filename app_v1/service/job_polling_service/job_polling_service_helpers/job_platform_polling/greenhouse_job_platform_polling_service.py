5227a5ec70c01607255c3bc282ce95dc50a0186b
Mahesh1choudhary <maheshchoudhary67894@gmail.com>
2026-04-11 00:04:47 +0530

    extra logs added

--- app_v1/service/job_polling_service/job_polling_service_helpers/job_platform_polling/greenhouse_job_platform_polling_service.py
+++ app_v1/service/job_polling_service/job_polling_service_helpers/job_platform_polling/greenhouse_job_platform_polling_service.py
@@ -12,6 +12,7 @@ class GreenhouseJobPlatformPollingService(JobPlatformPollingService):
 
     async def fetch_job_data_for_company(self, company_job_source:CompanyJobSourceModel) -> List[JobCreationRequest]:
         try:
+            logger.info(f"[{self.__class__.__name__}]-[{self.fetch_job_data_for_company.__name__}]: fetching jobs for company_job_source: {company_job_source}")
             fetch_job_list_url = company_job_source.fetch_job_list_url
             if fetch_job_list_url is None:
                 logger.warning(f"fetch_job_url not set for company_id: {company_job_source.company_id}")
@@ -33,19 +34,22 @@ class GreenhouseJobPlatformPollingService(JobPlatformPollingService):
             tasks = [self._process_job_data(job_data, company_job_source.company_id) for job_data in company_jobs_data]
             results = await asyncio.gather(*tasks, return_exceptions=True)
             job_creation_requests = [req for req in results if req is not None]
+            logger.info(f"[{self.__class__.__name__}]-[{self.fetch_job_data_for_company.__name__}]: fetched {len(job_creation_requests)} valid jobs for company_job_source: {company_job_source}")
             return job_creation_requests
         except Exception as exc:
-            logger.error(f"Error in {self.fetch_job_data_for_company.__name__}", exc_info=exc)
+            logger.error(f"[{self.__class__.__name__}]-[{self.fetch_job_data_for_company.__name__}]: error fetching jobs for company_job_source: {company_job_source}", exc_info=exc)
             return []
 
 
 
     async def _process_job_data(self, job_data: dict, job_company_id: int) -> JobCreationRequest:
         try:
+            logger.info(f"[{self.__class__.__name__}]-[{self._process_job_data.__name__}]: processing job for company_id: {job_company_id}")
             company_specific_job_id = job_data.get("internal_job_id", None)
             # TODO: this is compulsory as of now -> as we define uniqueness of job based on this only in database
             #TODO: will ignore jobs wihtout internal job id for now
             if company_specific_job_id is None:
+                logger.info(f"[{self.__class__.__name__}]-[{self._process_job_data.__name__}]: company_specific_job_id is None for job for company_id: {job_company_id}")
                 return None
 
             job_link = job_data.get("absolute_url", None)
@@ -55,7 +59,7 @@ class GreenhouseJobPlatformPollingService(JobPlatformPollingService):
             job_description = job_description + f"job content data : {job_data.get("content", None)}"
             return JobCreationRequest(job_company_id=job_company_id, job_internal_id=company_specific_job_id, job_link=job_link, job_description=job_description)
         except Exception as exc:
-            logger.error(f"Error in {self._process_job_data.__name__} for job_data:{job_data}", exc_info=True)
+            logger.error(f"[{self.__class__.__name__}]-[{self._process_job_data.__name__}]: Error processing job for company_id:{job_company_id}", exc_info=True)
             return None
 
 
